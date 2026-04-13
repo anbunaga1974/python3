46e6462f6d1abc692cf32f6edeba5d01a1de11ff
anbunaga1974 <anbunaga1974_bcs29@mepcoeng.ac.in>
2026-04-13 12:08:11 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+like = set(map(int, input().split()))
+dislike = set(map(int, input().split()))
+print(sum((i in like) - (i in dislike) for i in arr))
